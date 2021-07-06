7670aeeb6de6f3e30023b68539e6131204091de2
Nicholas Gan <nicholasganshyan@gmail.com>
2021-07-06 20:27:11 +0800

    update style
    Signed-off-by: Nicholas Gan <nicholasganshyan@gmail.com>

--- include/rmf_lift_scheduler/Database.hpp
+++ include/rmf_lift_scheduler/Database.hpp
@@ -18,89 +18,90 @@ template<typename State_Type, typename Hash_Type, typename Equal_Type>
 class Database
 {
 public:
-	using Impl_Val_Type = State_Type;
-	using Impl_DB_Type = std::unordered_set<Impl_Val_Type, Hash_Type, Equal_Type>;
-	using unique_ptr = std::unique_ptr<Database>;
+  using Impl_Val_Type = State_Type;
+  using Impl_DB_Type = std::unordered_set<Impl_Val_Type, Hash_Type, Equal_Type>;
+  using unique_ptr = std::unique_ptr<Database>;
 
-	/// \brief Construct a database object by populating it
-	/// \tparam InputIterator
-	/// \param first
-	/// \param last
-	/// \return std::unique_ptr of a database instance
-	template<typename InputIterator>
-	static unique_ptr
-		make(InputIterator first, InputIterator last);
+  /// \brief Construct a database object by populating it
+  /// \tparam InputIterator
+  /// \param first
+  /// \param last
+  /// \return std::unique_ptr of a database instance
+  template<typename InputIterator>
+  static unique_ptr
+  make(InputIterator first, InputIterator last);
 
-	/// \brief Get all the states in the database
-	/// \param container A vector to store the states in
-	void all(std::vector<Impl_Val_Type>& container) const
-	{
-		container.clear();
-		container.reserve(internal_db.size());
-		for(auto it = internal_db.cbegin(); it != internal_db.end(); ++it)
-		{
-			container.push_back(*it);
-		}
-	}
+  /// \brief Get all the states in the database
+  /// \param container A vector to store the states in
+  void all(std::vector<Impl_Val_Type>& container) const
+  {
+    container.clear();
+    container.reserve(internal_db.size());
+    for (auto it = internal_db.cbegin(); it != internal_db.end(); ++it)
+    {
+      container.push_back(*it);
+    }
+  }
 
-	/// \brief Get a reference to a lift's state. Matching is done using Equal_Type
-	/// \param val Element of type Impl_Val_Type that is used to find lift state
-	/// \return Reference to the state of the specified lift
-	/// \throws std::runtime_error if specified lift name cannot be found
-	Impl_Val_Type& find(const Impl_Val_Type& val)
-	{
-		auto result = internal_db.find(val);
-		if(result != internal_db.end())
-		{
-			return *result;
-		}
-		throw std::runtime_error("Lift not found");
-	}
+  /// \brief Get a reference to a lift state. Matching is done using Equal_Type
+  /// \param val Element of type Impl_Val_Type that is used to find lift state
+  /// \return Reference to the state of the specified lift
+  /// \throws std::runtime_error if specified lift name cannot be found
+  Impl_Val_Type& find(const Impl_Val_Type& val)
+  {
+    auto result = internal_db.find(val);
+    if (result != internal_db.end())
+    {
+      return *result;
+    }
+    throw std::runtime_error("Lift not found");
+  }
 
-	/// \brief Update the state of a lift.
-	/// \param state New state to update
-	void update(const Impl_Val_Type& state)
-	{
-		Impl_Val_Type& ref = find(state);
-		ref.update(state);
-	}
+  /// \brief Update the state of a lift.
+  /// \param state New state to update
+  void update(const Impl_Val_Type& state)
+  {
+    Impl_Val_Type& ref = find(state);
+    ref.update(state);
+  }
 
-	/// \brief Remove a lift. Matching is done using Equal_Type
-	/// \param lift Lift to remove
-	/// \return true if removed
-	bool remove_lift(const Impl_Val_Type& lift)
-	{
-		return internal_db.erase(lift) == 1;
-	}
+  /// \brief Remove a lift. Matching is done using Equal_Type
+  /// \param lift Lift to remove
+  /// \return true if removed
+  bool remove_lift(const Impl_Val_Type& lift)
+  {
+    return internal_db.erase(lift) == 1;
+  }
 
-	/// \brief Insert a lift into the database.
-	/// \param lift Lift to insert
-	void insert_lift(const Impl_Val_Type& lift)
-	{
-		internal_db.insert(lift);
-	}
+  /// \brief Insert a lift into the database.
+  /// \param lift Lift to insert
+  void insert_lift(const Impl_Val_Type& lift)
+  {
+    internal_db.insert(lift);
+  }
 
 private:
-	// Keep track of the state of all the lift cabins
-	Impl_DB_Type internal_db;
+  // Keep track of the state of all the lift cabins
+  Impl_DB_Type internal_db;
 
-	/// \brief Private constructor. Use the static make(...) function
-	///        to construct an instance of the database class.
-	template<typename InputIterator>
-	Database(InputIterator first, InputIterator last)
-	: internal_db(first, last)
-	{}
+  /// \brief Private constructor. Use the static make(...) function
+  ///        to construct an instance of the database class.
+  template<typename InputIterator>
+  Database(InputIterator first, InputIterator last)
+  : internal_db(first, last)
+  {}
 };
 
 template<typename State_Type, typename Hash_Type, typename Equal_Type>
 template<typename InputIterator>
 std::unique_ptr<Database<State_Type, Hash_Type, Equal_Type>>
-	Database<State_Type, Hash_Type, Equal_Type>::make(InputIterator first, InputIterator last)
+Database<State_Type, Hash_Type, Equal_Type>::make(InputIterator first,
+  InputIterator last)
 {
-	std::unique_ptr<Database<State_Type, Hash_Type, Equal_Type>> db_ptr(
-		new Database(first, last)
-	);
-	return db_ptr;
+  std::unique_ptr<Database<State_Type, Hash_Type, Equal_Type>> db_ptr(
+    new Database(first, last)
+  );
+  return db_ptr;
 }
 
 }
--- include/rmf_lift_scheduler/State.hpp
+++ include/rmf_lift_scheduler/State.hpp
@@ -10,26 +10,27 @@ namespace scheduler {
 /// \brief Base class which represents the current state of a lift cabin.
 ///        This represents a wrapper around the standard rmf_lift_msgs
 ///        state with other useful fields for calculating cost.
-///				 Inherit from this class to provide a custom implementation of the lift state.
+///		   Inherit from this class to provide a custom implementation of 
+///        the lift state.
 ///        The only other requirement is that there must be an update function
 ///        which takes in a const reference to the derived class object.
 struct State_Base
 {
-	State_Base(
-		const rmf_lift_msgs::msg::LiftState& lift_state
-	) :
-	rmf_lift_state(lift_state)
-	{}
+  State_Base(
+    const rmf_lift_msgs::msg::LiftState& lift_state
+  )
+  : rmf_lift_state(lift_state)
+  {}
 
-	rmf_lift_msgs::msg::LiftState rmf_lift_state;
+  rmf_lift_msgs::msg::LiftState rmf_lift_state;
 
-	/// \brief Update the state using another state object
-	/// \param other Reference state object to update to.
-	void update(const rmf_lift_msgs::msg::LiftState& other)
-	{
-		// todo : remove unnecessary copying
-		rmf_lift_state = other;
-	}
+  /// \brief Update the state using another state object
+  /// \param other Reference state object to update to.
+  void update(const rmf_lift_msgs::msg::LiftState& other)
+  {
+    // todo : remove unnecessary copying
+    rmf_lift_state = other;
+  }
 
 };
 
